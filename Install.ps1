4288eb7a5dea8173fa932e2b2376903e8d441d21
jepperaskdk <jepperaskdk@gmail.com>
2023-04-21 09:37:08 +0200

    Create Install.ps1

---
+++ Install.ps1
@@ -0,0 +1,10 @@
+winget install JanDeDobbeleer.OhMyPosh -s winget
+
+# Install font
+oh-my-posh font install Meslo
+
+# Terminal icons
+Install-Module -Name Terminal-Icons -Repository PSGallery
+
+# Autocomplete list
+Install-Module PSReadLine -AllowPrerelease -Force
